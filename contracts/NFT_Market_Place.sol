4ba8266a35ec188e27c12c07736e694404023afc
Osiyomeoh <samuelaleonomoh5@gmail.com>
2024-09-20 18:35:38 +0100

    chore: added the grant minter role function

--- contracts/NFT_Market_Place.sol
+++ contracts/NFT_Market_Place.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract NFTMarketplace is ERC721URIStorage, Ownable {
+contract NFTMarketplace is ERC721URIStorage, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     uint256 private _nextTokenId;
+    address private _owner;
 
     struct NFTListing {
         uint256 price;
@@ -18,17 +20,29 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
 
     event NFTListed(uint256 tokenId, uint256 price, address seller);
     event NFTSold(uint256 tokenId, uint256 price, address seller, address buyer);
+    event MinterAdded(address minter);
+    event MinterRemoved(address minter);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    constructor() ERC721("NFTMarketplace", "NFTM") {}
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        _owner = msg.sender;
+        _grantRole(MINTER_ROLE, msg.sender);
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
 
-    function mint(string memory tokenURI) public returns (uint256) {
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Caller is not the owner");
+        _;
+    }
+
+    function mint(address to, string memory tokenURI) external onlyRole(MINTER_ROLE) returns (uint256) {
         uint256 tokenId = _nextTokenId++;
-        _safeMint(msg.sender, tokenId);
+        _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
         return tokenId;
     }
 
-    function listNFT(uint256 tokenId, uint256 price) public {
+    function listNFT(uint256 tokenId, uint256 price) external {
         require(ownerOf(tokenId) == msg.sender, "Not the owner");
         require(price > 0, "Price must be greater than zero");
 
@@ -36,7 +50,7 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
         emit NFTListed(tokenId, price, msg.sender);
     }
 
-    function buyNFT(uint256 tokenId) public payable {
+    function buyNFT(uint256 tokenId) external payable {
         NFTListing memory listing = _listings[tokenId];
         require(listing.price > 0, "NFT not for sale");
         require(msg.value >= listing.price, "Insufficient payment");
@@ -55,18 +69,40 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
         emit NFTSold(tokenId, price, seller, msg.sender);
     }
 
-    function cancelListing(uint256 tokenId) public {
+    function cancelListing(uint256 tokenId) external {
         require(_listings[tokenId].seller == msg.sender, "Not the seller");
         delete _listings[tokenId];
     }
 
-    function getListing(uint256 tokenId) public view returns (NFTListing memory) {
+    function getListing(uint256 tokenId) external view returns (NFTListing memory) {
         return _listings[tokenId];
     }
 
-    function withdraw() public onlyOwner {
+    function grantMinterRole(address minter) external onlyOwner {
+        grantRole(MINTER_ROLE, minter);
+        emit MinterAdded(minter);
+    }
+
+    function revokeMinterRole(address minter) external onlyOwner {
+        revokeRole(MINTER_ROLE, minter);
+        emit MinterRemoved(minter);
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        _revokeRole(MINTER_ROLE, _owner);
+        _grantRole(MINTER_ROLE, newOwner);
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+    function owner() external view returns (address) {
+        return _owner;
+    }
+
+    function withdraw() external onlyOwner {
         uint256 balance = address(this).balance;
         require(balance > 0, "No balance to withdraw");
-        payable(owner()).transfer(balance);
+        payable(_owner).transfer(balance);
     }
 }
